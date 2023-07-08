cffeaf4fd6cdf73c415033a7c5e5cb8c6d68100a
SUSHAN BANIYA <sushanbaniya@gmail.com>
2023-07-08 20:30:44 +0545

    Adding Features

--- calculatorinc/Program.cs
+++ calculatorinc/Program.cs
@@ -6,7 +6,35 @@ namespace EntryPoint
     {
         public static void Main()
         {
-            Console.WriteLine("Hi");
+            Console.WriteLine("Enter First Number ");
+            string firstNum = Console.ReadLine();
+            int num1 = int.Parse(firstNum);
+
+            Console.WriteLine("Enter Operator ");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter Second Number ");
+            string secondNum = Console.ReadLine();
+            int num2 = int.Parse(secondNum);
+
+            if(op == "+")
+            {
+                int result = num1 + num2;
+                Console.WriteLine("Result of Calculation is: " + result);
+            } else if(op == "-")
+            {
+                Console.WriteLine("Result of Calculation is: " + (num1 - num2));
+            } else if(op == "/")
+            {
+                Console.WriteLine("Result of Calclulation is:  "+ (num1 / num2));
+            } else if(op == "*")
+            {
+                Console.WriteLine("Result of Calculation is: " + (num1 * num2));
+            } else
+            {
+                Console.WriteLine("Enter correct numbers and operators");
+            }
+
         }
     }
 }
