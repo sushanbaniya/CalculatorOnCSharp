2954731001d5f2aeaed8667ef1a597a74e67ea5c
SUSHAN BANIYA <sushanbaniya@gmail.com>
2023-07-12 17:48:19 +0545

    Added feature of Starting the Application on pressing S

--- calculatorinc/Program.cs
+++ calculatorinc/Program.cs
@@ -6,35 +6,79 @@ namespace EntryPoint
     {
         public static void Main()
         {
-            Console.WriteLine("Enter First Number ");
-            string firstNum = Console.ReadLine();
-            int num1 = int.Parse(firstNum);
 
-            Console.WriteLine("Enter Operator ");
-            string op = Console.ReadLine();
+            //Calculate();
 
-            Console.WriteLine("Enter Second Number ");
-            string secondNum = Console.ReadLine();
-            int num2 = int.Parse(secondNum);
+            bool login = false;
 
-            if(op == "+")
+            Console.WriteLine("Welecome to the Calculator App !!, Press 'S' and hit Enter to Start the Applicatioin");
+            string input = Console.ReadLine();
+
+            if (input == "s" || input == "S")
             {
-                int result = num1 + num2;
-                Console.WriteLine("Result of Calculation is: " + result);
-            } else if(op == "-")
+                Calculate();
+            }
+
+            else
             {
-                Console.WriteLine("Result of Calculation is: " + (num1 - num2));
-            } else if(op == "/")
+                Console.WriteLine("!! Calculator Starts only after Pressing 'S' and hitting enter !!");
+            }
+
+
+
+
+        }
+
+        public static void Calculate() {
+            //bool login = false;
+
+
+            //if (input == "s" || input == "S" || login == true)
+            //{
+                Console.WriteLine("Enter First Number ");
+                string firstNum = Console.ReadLine();
+
+                int num1 = int.Parse(firstNum);
+
+
+                Console.WriteLine("Enter Operator ");
+                string op = Console.ReadLine();
+
+                Console.WriteLine("Enter Second Number ");
+                string secondNum = Console.ReadLine();
+                int num2 = int.Parse(secondNum);
+
+                if (op == "+")
+                {
+                    int result = num1 + num2;
+                    Console.WriteLine("Result of Calculation is: " + result);
+                }
+                else if (op == "-")
+                {
+                    Console.WriteLine("Result of Calculation is: " + (num1 - num2));
+                }
+                else if (op == "/")
+                {
+                    Console.WriteLine("Result of Calclulation is:  " + (num1 / num2));
+                }
+                else if (op == "*")
+                {
+                    Console.WriteLine("Result of Calculation is: " + (num1 * num2));
+                }
+                else
+                {
+                    Console.WriteLine("Enter correct numbers and operators !!");
+                }
+
+            Console.WriteLine("Do you want to use calculator again? If yes press 'S' and hit enter");
+            string secondInput = Console.ReadLine();
+            if (secondInput == "S" || secondInput == "s")
             {
-                Console.WriteLine("Result of Calclulation is:  "+ (num1 / num2));
-            } else if(op == "*")
-            {
-                Console.WriteLine("Result of Calculation is: " + (num1 * num2));
-            } else
-            {
-                Console.WriteLine("Enter correct numbers and operators");
+                Main();
             }
 
         }
+
     }
+    
 }
